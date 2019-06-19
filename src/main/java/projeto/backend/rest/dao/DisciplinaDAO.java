87341126ff37c64dae4f56f26cf0a849e55a0b45
Rafaeldsa <rafael.azevedo@ccc.ufcg.edu.br>
2019-06-19 15:39:09 -0300

    disciplinas cadastradas

--- src/main/java/projeto/backend/rest/dao/DisciplinaDAO.java
+++ src/main/java/projeto/backend/rest/dao/DisciplinaDAO.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import projeto.backend.rest.model.Disciplina;
-import projeto.backend.rest.model.Usuario;
 
 import java.util.List;
 
@@ -14,11 +13,9 @@ import java.util.List;
 public interface DisciplinaDAO extends JpaRepository<Disciplina, String> {
 
 
-    @Override
-    <S extends Disciplina> List<S> saveAll(Iterable<S> entities);
+    List<Disciplina> saveAll(Iterable disciplinaLista);
 
-    @Query(value = "Select d from Disciplina as d where d.id=:did")
-    Disciplina findById(@Param("did") long id);
+    Disciplina findById(long id);
 
 
     List<Disciplina> findAll();
